533d6e313638f9065bddecb9914135408e8a5654
Pranay.Jain <pranay.jain@e-zest.in>
2023-10-17 14:26:08 +0530

    foodie

--- app/src/main/java/com/example/foodie/MainActivity.kt
+++ app/src/main/java/com/example/foodie/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -19,15 +20,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-      binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+      //binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        navController = findNavController(R.id.navHostFragment)
-        val appBarConfiguration= AppBarConfiguration(setOf(R.id.recipesFragment, R.id.favoriteRecipesFragment, R.id.foodJokeFragment))
+       //navController = findNavController(R.id.navHostFragment)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+         navController = navHostFragment.navController
+
+        val appBarConfiguration= AppBarConfiguration(
+            setOf(R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment))
 
         binding.bottomNavigationView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
